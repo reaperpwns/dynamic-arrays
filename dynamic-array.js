db545f3d04b47aff8f0d2bb84e9273735be1349c
nick <nickadaniya@gmail.com>
2022-08-30 12:52:34 -1000

    read and unshift

--- dynamic-array.js
+++ dynamic-array.js
@@ -1,13 +1,17 @@
 class DynamicArray {
 
-  constructor(defaultSize=4) {
-
-    // Your code here
+  constructor(defaultSize = 4) {
+    this.defaultSize = defaultSize
+    this.data = new Array(defaultSize)
+    this.capacity = defaultSize
+    this.length = 0
   }
 
   read(index) {
-
-    // Your code here
+    if (index >= this.length) {
+      return undefined
+    }
+    return this.data[index]
   }
 
   push(val) {
@@ -27,16 +31,19 @@ class DynamicArray {
   }
 
   unshift(val) {
+    for (let i = this.length; i > 0; i--) {
+      this.data[i] = this.data[i - 1]
+    }
+    this.data[0] = val
+    this.length++
+  }
+
+  indexOf(val) {
 
     // Your code here
   }
 
-  indexOf (val) {
-
-    // Your code here
-  }
-
-  resize () {
+  resize() {
 
     // Your code here
   }
